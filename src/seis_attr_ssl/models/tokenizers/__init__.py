576cabbe4b296752365125f8bd46c3cc140b054e
ozw4 <zerowarurei@gmail.com>
2026-06-11 04:08:43 +0000

    chore: address issue #33

--- src/seis_attr_ssl/models/tokenizers/__init__.py
+++ src/seis_attr_ssl/models/tokenizers/__init__.py
@@ -1,3 +1,7 @@
 """Tokenizer model components."""
 
-__all__: list[str] = []
+from seis_attr_ssl.models.tokenizers.attribute_patch_tokenizer3d import (
+	AttributePatchTokenizer3D,
+)
+
+__all__ = ['AttributePatchTokenizer3D']
